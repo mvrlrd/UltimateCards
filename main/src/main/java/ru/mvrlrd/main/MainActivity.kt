d8b3d11b82f1bf80d4723e67ab332a6c13ebaa96
mvrlrd <mvrlrd@yandex.ru>
2024-01-28 18:12:19 +0300

    separate category nav graph from main activity graph

--- main/src/main/java/ru/mvrlrd/main/MainActivity.kt
+++ main/src/main/java/ru/mvrlrd/main/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_category, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.category_graph, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
