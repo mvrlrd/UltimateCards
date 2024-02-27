6b7ef9d2bc361e7c74d34669ef523278465aff16
mvrlrd <mvrlrd@yandex.ru>
2024-02-27 22:03:12 +0300

    add recycler in fragment

--- modules/feature/add_category/src/main/java/ru/mvrlrd/add_category/AddCategoryFragment.kt
+++ modules/feature/add_category/src/main/java/ru/mvrlrd/add_category/AddCategoryFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import ru.mvrlrd.add_category.databinding.FragmentAddCategoryBinding
 
-
 class AddCategoryFragment : Fragment() {
+    private val iconAdapter = IconAdapter()
 
     private var _binding: FragmentAddCategoryBinding? = null
     private val binding: FragmentAddCategoryBinding get() = _binding?:throw RuntimeException("FragmentAddCategoryBinding is null")
@@ -20,6 +21,14 @@ class AddCategoryFragment : Fragment() {
     ): View {
         _binding = FragmentAddCategoryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvIcons.apply {
+            adapter = iconAdapter
+            layoutManager = GridLayoutManager(requireContext(), 3, GridLayoutManager.VERTICAL, false)
+        }
     }
 
     override fun onDestroyView() {
