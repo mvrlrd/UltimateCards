d68d4246a844a1d7054d333a436a0d562182d081
mvrlrd <mvrlrd@yandex.ru>
2024-02-28 15:29:46 +0300

    edit fragment to use adapter

--- modules/feature/add_category/src/main/java/ru/mvrlrd/add_category/AddCategoryFragment.kt
+++ modules/feature/add_category/src/main/java/ru/mvrlrd/add_category/AddCategoryFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import ru.mvrlrd.add_category.databinding.FragmentAddCategoryBinding
 
 class AddCategoryFragment : Fragment() {
-    private val iconAdapter = IconAdapter()
+    private val iconAdapter = IconAdapter{pos->
+        onListItemClick(pos)
+    }
 
     private var _binding: FragmentAddCategoryBinding? = null
     private val binding: FragmentAddCategoryBinding get() = _binding?:throw RuntimeException("FragmentAddCategoryBinding is null")
 
+    private fun onListItemClick(position: Int) {
+        Toast.makeText(requireContext(), getIcons()[position].res, Toast.LENGTH_SHORT).show()
+        val newList = getIcons() as MutableList
+        newList[position] = newList[position].copy(isSelected = true)
+        iconAdapter.items = newList
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +38,22 @@ class AddCategoryFragment : Fragment() {
             adapter = iconAdapter
             layoutManager = GridLayoutManager(requireContext(), 3, GridLayoutManager.VERTICAL, false)
         }
+        iconAdapter.items = getIcons()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+   private fun getIcons(): List<IconItem> {
+        return listOf(
+            IconItem(R.drawable.abstract_shape_04, false),
+            IconItem(R.drawable.abstract_shape_05, false),
+            IconItem(R.drawable.abstract_shape_10, false),
+            IconItem(R.drawable.abstract_shape_11, false),
+            IconItem(R.drawable.abstract_shape_13, false),
+        )
     }
 
     companion object {
